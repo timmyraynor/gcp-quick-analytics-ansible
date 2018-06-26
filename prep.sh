097a0febad92717f1647ef5ac8dbf445193570ae
Tim Qin <qinyujue@gmail.com>
2018-06-26 15:22:04 +1000

    Add submodule keys

--- prep.sh
+++ prep.sh
@@ -1,2 +1,4 @@
 git config --global user.email "qinyujue@gmail.com"
 git config --global user.name "Tim Qin"
+
+gcloud compute --project $PROJECT_NAME instances create "ansible-master" --zone "us-central1-b" --machine-type "g1-small" --network "default" --maintenance-policy "MIGRATE" --scopes "https://www.googleapis.com/auth/userinfo.email" "https://www.googleapis.com/auth/compute" "https://www.googleapis.com/auth/devstorage.full_control" --tags "http-server" "https-server" --image "https://www.googleapis.com/compute/v1/projects/centos-cloud/global/images/centos-7-v20140926" --no-boot-disk-auto-delete
