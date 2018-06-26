c47a8df224efc3d29f793eb045b99e24b6b23182
Tim Qin <qinyujue@gmail.com>
2018-06-26 15:25:25 +1000

    Change the prep script to enable a vm for ansible connections

--- prep.sh
+++ prep.sh
@@ -1,4 +1,10 @@
-git config --global user.email "qinyujue@gmail.com"
-git config --global user.name "Tim Qin"
+#!/bin/bash
+echo "Input Project_ID:"
+read project_name
+echo "Input service account P12 key path:"
+read p12key_path
 
-gcloud compute --project $PROJECT_NAME instances create "ansible-master" --zone "us-central1-b" --machine-type "g1-small" --network "default" --maintenance-policy "MIGRATE" --scopes "https://www.googleapis.com/auth/userinfo.email" "https://www.googleapis.com/auth/compute" "https://www.googleapis.com/auth/devstorage.full_control" --tags "http-server" "https-server" --image "https://www.googleapis.com/compute/v1/projects/centos-cloud/global/images/centos-7-v20140926" --no-boot-disk-auto-delete
+gcloud compute --project $project_name instances create "ansible-master" --zone "us-central1-b" --machine-type "g1-small" --network "default" --maintenance-policy "MIGRATE" --scopes "https://www.googleapis.com/auth/userinfo.email" "https://www.googleapis.com/auth/compute" "https://www.googleapis.com/auth/devstorage.full_control" --tags "http-server" "https-server" --image "https://www.googleapis.com/compute/v1/projects/centos-cloud/global/images/centos-7-v20140926" --no-boot-disk-auto-delete
+openssl pkcs12 -in $p12key_path -passin pass:notasecret -nodes -nocerts | openssl rsa -out gce-key.pem
+gcutil --permit_root push gce-key.pem /root
+gcloud compute ssh ansible
