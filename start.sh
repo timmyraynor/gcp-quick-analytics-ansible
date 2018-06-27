e490492dc7b75b6e7c9643dad060048638dd0bba
Tim Qin <qinyujue@gmail.com>
2018-06-27 21:46:54 +1000

    Disable gather facts as that will lead to failure

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-ansible-playbook gce-instances.yml --user tim_qin_yujue_gmail_com --private-key gcp_vars/secrets/service-ansible-key
+ansible-playbook -e 'host_key_checking=False' gce-instances.yml --user tim_qin_yujue_gmail_com --private-key gcp_vars/secrets/service-ansible-key
